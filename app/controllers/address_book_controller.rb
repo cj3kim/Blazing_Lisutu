66b10a71c01e7359e6f787eccfd02dc718be6baa
Chris <cj3kim@gmail.com>
2012-06-19 14:20:43 -0700

    Attendees can now be created

--- app/controllers/address_book_controller.rb
+++ app/controllers/address_book_controller.rb
@@ -7,4 +7,27 @@ class AddressBookController < ApplicationController
   def new
     @person = AddressBook.new
   end
+
+  def create
+    @person = AddressBook.new(params[:post]) 
+
+    if @person.save 
+      redirect_to root_url, notice: 'Person has been added'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @person = AddressBook.find(params[:id])
+
+  end
+
+  def update
+    
+  end
+
+  def destroy
+
+  end
 end
