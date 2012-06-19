eed5e5c40ee72f7ca41f11921d09f827dbb7f11a
Chris <cj3kim@gmail.com>
2012-06-19 10:37:29 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lisutu::Application.config.secret_token = 'a23591d88a3c5dabc0a8253223f8a619014e75a03e40a68785e8f76b14d55be7d505b70929187150705403a24352d2da1454970b4204cec6e1fb1ef9b545b824'
