1953633a944a7d132d7ee3374d1aefaac01b0174
Chris <cj3kim@gmail.com>
2012-07-13 11:07:34 -0700

    Added four helper methods to viewhelpers at the bottom of the code

--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -1,3 +1,6 @@
+
+// String representation of a generic person form
+// Could be used as an edit or new form
 var personForm = "<form><br /> \
                   <label>First Name</label>  \
                   <input  class='f_name' type='text' value=''></input> <br><br>  \
@@ -10,32 +13,37 @@ var personForm = "<form><br /> \
                   <input type='submit'></input> <br /> \
                   </form>";  
 
+//dropDownBox is  string representation of a row for editing 
+//the selected person
 var dropDownBox = "<tr class='person' id='' + personId + 'n'><td colspan=6> <div class='form_box'></div></td></tr>";
 
+//The view helper object. It interacts with the view (the DOM Tree)
+//to make changes.
 var viewHelper = {};
 
+//Slides down an edit form
 viewHelper.slideEditForm = function ($personTr) {
 
+  //Deduce personId from $personTr
   var personId = $personTr.attr('id');
+  //Encapsulates a personForm into a jQuery object
   var $editForm = $(personForm);
   
-  function findEditFormContainer() {
-    return $personTr.next().find('div.form_box');
-  }
+  //If the div, which contains the form, is empty, insert an
+  //edit form into the div. 
+  if (findEditFormContainer($personTr).length === 0) {
 
-  if (findEditFormContainer().length === 0) {
-
+    //Make a GET request for person data
     var personData = modelMethods.getPerson(personId); 
 
-    this.insertEditForm($personTr, $editForm);// insert edit form
-    this.updateForm($editForm, personData)//update edit form
-
+    //Insert an edit row after the personTr and then insert an edit form
+    this.insertEditForm($personTr, $editForm);
+    //Insert person data from the GET request in the form
+    this.updateForm($editForm, personData)
+    //Place an the submit handler on the edit form
     viewController.forSubmitOnEditForm( personId, $editForm);
 
-    var $dropDownBox = $personTr.next(); //I know this smells!
-
-    var $divInDropDownBox = $dropDownBox.find('div.form_box');
-    $divInDropDownBox.hide().slideDown(800);
+    findEditFormContainer($personTr).hide().slideDown(800);
 
   } else {
       var $dropDownBox = $personTr.next(); 
@@ -101,7 +109,12 @@ viewHelper.updatePersonRow = function (callback, data) {
     callback.find('#phone_num').text(data.person.phone_num); 
 }
 
-
+function findEditForm($personTr) {
+    return $personTr.next().find('form');
+}
+function findEditFormContainer($personTr) {
+    return $personTr.next().find('div.form_box');
+}
 function findNewFormContainer() {
   return $('center#new_person div.form_box');
 }
